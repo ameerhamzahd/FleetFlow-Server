f6b958f16001d0fe0b97ffa80cf31b9a0f2faa08
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-02-23 02:37:39 +0600

    FleetFlow-Server users-table-creation Done.

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,19 @@ export const pool = new Pool({
 });
 
 const initDB = async() => {
-    await pool.query(``);
+    await pool.query(`
+        CREATE TYPE user_role AS ENUM ('admin', 'customer');
+
+        CREATE TABLE IF NOT EXISTS users(
+        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name VARCHAR(200) NOT NULL,
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
+        phone VARCHAR(20) NOT NULL,
+        role user_role NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )`);
 };
 
 export default initDB;
