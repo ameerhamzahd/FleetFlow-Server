37125c8cdb2cc4f11d14414aaf509b44b0556e79
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-02-23 03:20:03 +0600

    FleetFlow-Server bookings-table-creation Done.

--- src/config/db.ts
+++ src/config/db.ts
@@ -9,7 +9,7 @@ export const pool = new Pool({
 const initDB = async() => {
     await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
-        id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         name VARCHAR(200) NOT NULL,
         email VARCHAR(200) NOT NULL UNIQUE,
         password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
@@ -21,13 +21,24 @@ const initDB = async() => {
 
     await pool.query(`
         CREATE TABLE IF NOT EXISTS vehicles(
-        id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         vehicle_name VARCHAR(200) NOT NULL,
         type VARCHAR(20) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
         registration_number VARCHAR(100) NOT NULL UNIQUE,
         daily_rent_price NUMERIC(10, 2) NOT NULL CHECK (daily_rent_price > 0),
         availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked')) DEFAULT 'available'
-        )`)
+        )`);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS bookings(
+        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL,
+        total_price NUMERIC(10, 2) NOT NULL CHECK (total_price > 0),
+        status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned'))
+        )`);
 };
 
 export default initDB;
