9f0adccfcc41f53b84d75029f56ef90700263cda
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-02-23 03:07:33 +0600

    FleetFlow-Server vehicles-table-creation Done.

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,18 +8,26 @@ export const pool = new Pool({
 
 const initDB = async() => {
     await pool.query(`
-        CREATE TYPE user_role AS ENUM ('admin', 'customer');
-
         CREATE TABLE IF NOT EXISTS users(
-        id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         name VARCHAR(200) NOT NULL,
         email VARCHAR(200) NOT NULL UNIQUE,
         password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
         phone VARCHAR(20) NOT NULL,
-        role user_role NOT NULL,
+        role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer')),
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )`);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles(
+        id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        vehicle_name VARCHAR(200) NOT NULL,
+        type VARCHAR(20) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(100) NOT NULL UNIQUE,
+        daily_rent_price NUMERIC(10, 2) NOT NULL CHECK (daily_rent_price > 0),
+        availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked')) DEFAULT 'available'
+        )`)
 };
 
 export default initDB;
