a6bc5c91eb8a2ed0bfc7aa0f92ee0b1824b6600f
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-03-04 17:16:43 +0600

    update: modify files

--- src/modules/vehicles/vehicle.routes.ts
+++ src/modules/vehicles/vehicle.routes.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { vehicleControllers } from "./vehicle.controllers";
+import { authorizeRole, verifyToken } from "../../middlewares/auth";
 
 const router = Router();
 
-router.post("/", vehicleControllers.createVehicle);
+router.post("/", verifyToken, authorizeRole("admin"), vehicleControllers.createVehicle);
 
 router.get("/", vehicleControllers.getAllVehicles);
 
 router.get("/:vehicleId", vehicleControllers.getSingleVehicle);
 
-router.put("/:vehicleId", vehicleControllers.updateVehicle);
+router.put("/:vehicleId", verifyToken, authorizeRole("admin"), vehicleControllers.updateVehicle);
 
-router.delete("/:vehicleId", vehicleControllers.deleteVehicle);
+router.delete("/:vehicleId", verifyToken, authorizeRole("admin"), vehicleControllers.deleteVehicle);
 
 
 export const vehiclesRoutes = router;
