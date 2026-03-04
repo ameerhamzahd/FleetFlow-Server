05ab7d74c4167dd330cf2e8453c69a356d10e0fd
Ameer Hamzah Daiyan <ameerhamzahdaiyan.02@gmail.com>
2026-03-04 17:20:59 +0600

    update: modify files

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { usersController } from "./user.controllers";
+import { authorizeRole, verifyToken } from "../../middlewares/auth";
 
 const router = Router();
 
-router.get("/", usersController.getAllUsers);
+router.get("/", verifyToken, authorizeRole("admin"), usersController.getAllUsers);
 
-router.put("/:userId", usersController.updateUser);
+router.put("/:userId", verifyToken, usersController.updateUser);
 
-router.delete("/:userId", usersController.deleteUser);
+router.delete("/:userId", verifyToken, authorizeRole("admin"), usersController.deleteUser);
 
 export const usersRoutes = router;
